16a96dfeeea457e34520e2a4aea470e5cfeaacdc
Seb U <sunipan@gmail.com>
2023-01-11 13:53:23 -0800

    okay apparently we need comments for code to work, thanks javascript.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -255,7 +255,59 @@ const Home: NextPage = () => {
               </li>
             </ul>
           </ExperienceItem>
-
+          {/* <div className="experience relative flex h-[120vh] w-full items-center justify-center overflow-hidden bg-dark text-3xl text-white xs:h-screen">
+            <div className="z-50 flex flex-col items-center justify-center">
+              <div className="flex h-52 w-52 items-center justify-center rounded-full bg-white">
+                <Image src="/assets/village.png" width={100} height={100} alt="" />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <a
+                  href="https://thevillageapp.co"
+                  rel="noreferrer"
+                  target="_blank"
+                  className="flex flex-row items-center justify-center gap-3 pt-3 hover:opacity-70"
+                >
+                  <h3 className="text-center text-2xl font-medium text-white sm:text-3xl">
+                    The Village App
+                  </h3>
+                  <Image
+                    className="invert"
+                    src="/assets/icons/newtab.png"
+                    height={20}
+                    width={20}
+                    alt=""
+                  />
+                </a>
+                <div className="w-full py-3 text-center text-base italic opacity-70 sm:text-lg">
+                  Junior Software Developer - Co-op
+                </div>
+                <p className="max-w-2xl px-5 text-center text-base sm:text-lg md:text-xl">
+                  A mobile app designed to bring communities together and help users find resources
+                  for support
+                </p>
+                <h4 className="mt-5 max-w-2xl text-center text-lg font-medium underline underline-offset-8 sm:text-xl md:text-2xl">
+                  Areas I impacted:
+                </h4>
+                <div className="flex justify-center px-10">
+                  <ul className="flex max-w-md list-disc flex-col gap-5 pt-2 text-base md:text-lg">
+                    <li>
+                      Refactored the entire front end within 4 months to increase maintainability
+                      and user experience by up to 80%.
+                    </li>
+                    <li>
+                      Corrected major bugs in the underlying structure of the application and
+                      reviewed multiple PRs by other team members.
+                    </li>
+                    <li>
+                      Introduced better state management libraries to increase development time by
+                      50%.
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="absolute inset-x-0 bottom-0 h-2/3 w-full bg-gradient-to-t from-[#1cb0ac] to-transparent"></div>
+          </div> */}
           <ExperienceItem
             src="/assets/cpd.png"
             title="Jusaves"
