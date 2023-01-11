3df2e3b6b752c3d51521db49f45b54b816f178b2
Seb U <sunipan@gmail.com>
2023-01-11 13:37:28 -0800

    deleted another comment, removed spacing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -157,7 +157,6 @@ const Home: NextPage = () => {
               </li>
             </ul>
           </ExperienceItem>
-
           <ExperienceItem
             src="/assets/minga.png"
             title="Minga"
@@ -183,53 +182,6 @@ const Home: NextPage = () => {
               </li>
             </ul>
           </ExperienceItem>
-          {/* <div className="experience relative flex h-[120vh] w-full items-center justify-center overflow-hidden bg-dark text-3xl text-white xs:h-screen">
-            <div className="z-50 flex flex-col items-center justify-center">
-              <div className="flex h-52 w-52 items-center justify-center rounded-full bg-[#1c2f59]">
-                <Image src="/assets/minga.png" width={130.8} height={34.8} alt="" />
-              </div>
-              <div className="flex flex-col items-center justify-center">
-                <a
-                  href="https://rentalhunt.ca/"
-                  rel="noreferrer"
-                  target="_blank"
-                  className="flex flex-row items-center justify-center gap-3 pt-3 hover:opacity-70"
-                >
-                  <h3 className="text-center text-2xl font-medium text-white sm:text-3xl">Minga</h3>
-                  <Image
-                    className="invert"
-                    src="/assets/icons/newtab.png"
-                    height={20}
-                    width={20}
-                    alt=""
-                  />
-                </a>
-                <div className="w-full py-3 text-center text-base italic opacity-70 sm:text-lg">
-                  Junior Software Engineer - Co-op
-                </div>
-                <p className="max-w-2xl px-5 text-center text-base sm:text-lg md:text-xl">
-                  A revolutionary new rental app that has your back
-                </p>
-                <h4 className="mt-5 max-w-2xl text-center text-lg font-medium underline underline-offset-8 sm:text-xl md:text-2xl">
-                  Areas I impacted:
-                </h4>
-                <div className="flex justify-center px-10">
-                  <ul className="flex max-w-md list-disc flex-col gap-5 pt-2 text-base sm:text-lg">
-                    <li>
-                      Independently implemented the app&apos;s critical new point allocation system
-                      used by over 10,000 users daily.
-                    </li>
-                    <li>Converted several UIs to updated designs with improved practices</li>
-                    <li>
-                      Learned Angular 9 and the gRPC protocol quickly in order to be a valuable team
-                      member for the app
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="absolute inset-x-0 bottom-0 h-2/3 w-full bg-gradient-to-t from-[#1c2f59] to-transparent"></div>
-          </div> */}
           <ExperienceItem
             role="Junior Software Engineer - Co-op"
             src="/assets/village.png"
